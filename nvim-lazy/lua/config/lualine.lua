12913a11a433cac261ff618884e55f56e54d71e9
Jonathan Sönnerup <nooxet@gmail.com>
2024-04-23 20:17:43 +0200

    Added lualine, and substitute plugins

---
+++ nvim-lazy/lua/config/lualine.lua
@@ -0,0 +1,11 @@
+require("lualine").setup({
+  sections = {
+    lualine_x = {
+      {
+        require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = { fg = "#ff9e64" },
+      }
+    },
+  },
+})
