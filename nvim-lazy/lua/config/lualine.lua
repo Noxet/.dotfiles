9585d4245b40a4d4587b4a17ad98f5ebdbd22256
Jonathan Sönnerup <nooxet@gmail.com>
2024-05-27 13:12:55 +0200

    Added empty extension field to lualine, for future use

--- nvim-lazy/lua/config/lualine.lua
+++ nvim-lazy/lua/config/lualine.lua
@@ -1,11 +1,13 @@
 require("lualine").setup({
-  sections = {
-    lualine_x = {
-      {
-        require("noice").api.statusline.mode.get,
-        cond = require("noice").api.statusline.mode.has,
-        color = { fg = "#ff9e64" },
-      }
+    sections = {
+        lualine_x = {
+            {
+                require("noice").api.statusline.mode.get,
+                cond = require("noice").api.statusline.mode.has,
+                color = { fg = "#ff9e64" },
+            }
+        },
     },
-  },
+    extensions = {
+    }
 })
